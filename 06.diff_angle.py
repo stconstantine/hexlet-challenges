1006c553200ae87cbfe7b2da3a81de089aa86bff
stconstantine <strkto@gmail.com>
2022-04-12 23:36:34 +0300

    added diff_angles

---
+++ 06.diff_angle.py
@@ -0,0 +1,15 @@
+# https://ru.hexlet.io/challenges/python_basics_angle_difference_exercise
+
+def reduct_angle(angle):
+    return angle % 360
+
+def diff(a1: int, a2: int):
+    a1 = reduct_angle(a1)
+    a2 = reduct_angle(a2)
+
+    straight_angle = max(a1, a2) - min(a1, a2)
+
+    return min(straight_angle, 360 - straight_angle)
+
+
+print(diff(315, 45))
