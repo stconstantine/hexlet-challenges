7935caaef122b87853244262c79e5c54547447b8
stconstantine <strkto@gmail.com>
2022-05-28 15:56:19 +0300

    added 7 and 8

--- 07.is_power_3.py
+++ 07.is_power_3.py
@@ -1,9 +1,31 @@
 # https://ru.hexlet.io/challenges/python_basics_power3_exercise
 
+from time import time
+
 def is_power_of_three(number):
     for i in range(0, int(pow(number, 1/3)+1)):
         if 3 ** i == number:
             return True
     return False
 
-print (is_power_of_three(26))
+def is_power_of_three_h(number):
+    counter = 1  # 3 ** 0
+    while counter < number:
+        counter *= 3
+    return counter == number
+
+N = 1
+while N < 1000000000000:
+    start_time = time()
+    is_power_of_three(N)
+    stop_time = time()
+    f1 = stop_time - start_time
+
+    start_time = time()
+    is_power_of_three_h(N)
+    stop_time = time()
+    f2 = stop_time - start_time
+
+    print(f"N = {N}, вычисление pow(.., 1/3) – {f1 * 1000000}, полный проход - {f2 * 1000000}")
+
+    N *= 1000
